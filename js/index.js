54f26df5108c44bb285cae6f9fd248b3cc946302
Pntrmln <166542356+Pntrmln@users.noreply.github.com>
2025-08-01 18:19:21 +0200

    Update JS: Home

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ function Redir(site){
         window.open("https://github.com/pntrmln");
     } else if (site == 'anime') {
         window.location.href = "anime.html";
+    } else if (site == 'japanese') {
+        window.location.href = "japanese.html"; 
     } else {
         window.location.href = "404.html";
     }
@@ -28,4 +30,46 @@ function checkMobile(){
     // AMÍG A PROJEKTEK NINCSENEK A HELYÉN
     let divMagassag = $("#rolam-div").outerHeight();
     $("#nothing-div").height(divMagassag);
+}
+function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth();
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
+function timeWrite(){
+    const jelenlegi_datum = new Date();
+    const jelenlegi_ev = jelenlegi_datum.getFullYear();
+    var g_honap = monthDiff(new Date("2024-09-01"), jelenlegi_datum);
+    var j_honap = g_honap - 4;
+    if (g_honap >= 12) {
+        let g_evek = Math.floor(g_honap / 12);
+        g_honap -= g_evek * 12;
+        if (g_honap != 0) {
+            $("#gido_hu").text("(" + g_evek + " éve és " + g_honap + " hónapja)");
+            $("#gido_en").text("(Since " + g_evek + " years and " + g_honap + " months)");
+        } else {
+            $("#gido_hu").text("(" + g_evek + " éve)");
+            $("#gido_en").text("(Since " + g_evek + " years)");
+        }
+    } else {
+        $("#gido_hu").text("(" + g_honap + " hónapja)");
+        $("#gido_en").text("(Since " + g_honap + " months)");
+    }
+    if (j_honap >= 12) {
+        let j_evek = Math.floor(j_honap / 12);
+        j_honap -= j_evek * 12;
+        if (j_honap != 0) {
+            $("#jido_hu").text("(" + j_evek + " éve és " + j_honap + " hónapja)");
+            $("#jido_en").text("(Since " + j_evek + " years and " + j_honap + " months)");
+        } else {
+            $("#jido_hu").text("(" + j_evek + " éve)");
+            $("#jido_en").text("(Since " + j_evek + " years)");
+        }
+    } else {
+        $("#jido_hu").text("(" + j_honap + " hónapja)");
+        $("#jido_en").text("(Since " + j_honap + " months)");
+    }
+    $(".ekor").text(jelenlegi_ev - 2008);
 }
