c37ad1a5ecc8dac7acee2d30fa760a1cc7aaadb0
Pntrmln <166542356+Pntrmln@users.noreply.github.com>
2025-09-11 17:51:48 +0200

    Update JS: Construction
    ez már egy jó ideje megvan, csak még nem commitoltam (08.05.) :D

--- js/construction.js
+++ js/construction.js
@@ -3,16 +3,16 @@ var magyar = false;
 function changeText(){
     if (!magyar) {
         if ($(window).width() < 600 ) {
-            $("#ct").html('<i class="fa fa-wrench"></i><br>Fejlesztés');
+            $("#ct").html('Fejlesztés');
         } else {
-            $("#ct").html('<i class="fa fa-wrench"></i> Weboldal fejlesztés alatt! <i class="fa fa-wrench"></i>');
+            $("#ct").html('Weboldal fejlesztés alatt!');
         }
         magyar = true;
     } else {
         if ($(window).width() < 600 ) {
-            $("#ct").html('<i class="fa fa-wrench"></i><br>Construction');
+            $("#ct").html('Construction');
         } else {
-            $("#ct").html('<i class="fa fa-wrench"></i> Website is under construction! <i class="fa fa-wrench"></i>');
+            $("#ct").html('Website is under construction!');
         }
         magyar = false;
     }
