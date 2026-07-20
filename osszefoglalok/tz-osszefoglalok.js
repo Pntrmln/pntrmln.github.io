6aaf24cefb4523260f58bc8a1e7b6bde578036bf
Pntrmln <166542356+Pntrmln@users.noreply.github.com>
2026-07-20 10:25:07 +0200

    tesztelés

--- osszefoglalok/tz-osszefoglalok.js
+++ osszefoglalok/tz-osszefoglalok.js
@@ -1,34 +1,3 @@
-// TÖRI DOWNLOAD LINKEK
-var tlink = {
-    "A nemzetállamok kora":["https://drive.google.com/uc?export=download&id=11UbLwLDbA_-Qg8vyG-C_dgVGg-0NxKf6"],
-    "Az 1848-49-es forradalom és szabadságharc":["https://drive.google.com/uc?export=download&id=1RPh9aSWky03D2u8YkSAkUCjEf_PQKv3o"],
-    "Magyarország a Habsburg Birodalomban":["https://drive.google.com/uc?export=download&id=1Dna4IA5zl7duqIOM-JgimjrBJ5sS5lTH"],
-    "A forradalmak kora":["https://drive.google.com/uc?export=download&id=16lSOP44Z2L2kpe2pn-nk62wHRX3io_bc"],
-    "A felvilágosodás kora":["https://drive.google.com/uc?export=download&id=1oMpx0PSSjHlX9b2rd6XzugqFZXq8lDt3"],
-    "A török hódoltság kora":["https://drive.google.com/uc?export=download&id=1IoIrKz5CG4XB8NONe1rYiTuESfs2tEMn"],
-    "A magyar királyság fénykora":["https://drive.google.com/uc?export=download&id=1Vjx7ckXXY_cTIBVBeOYf4Mxlrzkqx4ah"],
-    "A korai újkor":["https://drive.google.com/uc?export=download&id=1CIUCVSCYzQKMXwvqcfIt8V0acgDIsVI4"],
-    "Árpád-ház":["https://drive.google.com/uc?export=download&id=1dG4ZYfO5ELLppTjlRZA-xdFZsalmmUU4"],
-    "Honfoglalás":["https://drive.google.com/uc?export=download&id=1gGvcl3PyTI2RJqVtHbLkvraNZ1TAbHvs"],
-    "A középkor":["https://drive.google.com/uc?export=download&id=1Jy_RtCEHepLczYP6EskhovINdPlyBX_6"],
-    "Az ókori Róma":["https://drive.google.com/uc?export=download&id=1PRG9IxN9b0JA-KqdBm6R7oAVou9OklIm"],
-    // 2025/26
-    "A dualizmus kora":["https://drive.google.com/uc?export=download&id=1gldhgGZ997nCquT98BcFNIi9a7Ngrpq5"],
-    "Az I. világháború és következményei":["https://drive.google.com/uc?export=download&id=1H9Jv8kRaYc-oeGhP5hM5vsiuq0XdlUae"],
-    "Két világháború között":["https://drive.google.com/uc?export=download&id=1rSpctJ9Z66GWX_1r9BY7d0Jx29ZIcd0J"],
-    "A II. világháború":["https://drive.google.com/uc?export=download&id=1oZGqDhvye1YOHuUtEfS0C7qVO6NrhX6t"]
-};
-// MAGYAR DOWNLOAD LINKEK
-var mlink = {
-    "Magyar költők és műveik":["https://drive.google.com/uc?export=download&id=1WUrW-pO5k34E9-9BZpSDMlPnMPdZ41BS"],
-    "A szöveg":["https://drive.google.com/uc?export=download&id=15Prp--ESpPQBSCjAvJQMVYfSAnDF-Jac"],
-    "A romantika kora":["https://drive.google.com/uc?export=download&id=1Nb6RnCpxmBEWxpCl5NOa2pg211M6tmE7"],
-    "A felvilágosodás kora":["https://downgit.github.io/#/home?url=https://github.com/Pntrmln/pntrmln.github.io/blob/main/osszefoglalok/magyar/A felvilágosodás kora.pdf"],
-    "Csokonai költészete":["https://drive.google.com/uc?export=download&id=1cIgaR2ZWg7NEzTCblmravW81uMQsBZKe"],
-    // 2025/26
-    "Magyar belső vizsga":["https://drive.google.com/uc?export=download&id=10zCplxXMvxoHjtXunnWto-O-rTKPMwO6"],
-    "A 19. század második felének magyar irodalma":["/osszefoglalok/magyar/A 19. század második felének magyar irodalma.pdf"]
-}
 // Változók a Fájlkezelő kezeléséhez
 var oldal = location.href.split("/").slice(-1).toString();
 var utolso = '';
@@ -36,40 +5,27 @@ var bezarva = true;
 var auto_close = false;
 if (oldal.endsWith("html")){
     oldal = oldal.slice(0, -5); // .html levágása
-} 
+}
+
+async function PDFLetoltes(link, lecke) {
+    let valasz = await fetch(link);
+    let blob = await valasz.blob();
+    let blobURL = URL.createObjectURL(blob);
+    let a = document.createElement("a");
+    a.href = blobURL;
+    a.download = lecke;
+
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+    URL.revokeObjectURL(blobURL);
+}
+
 function createFileExplorer(lecke){
-    let link = "";
-    if (oldal == 'tortenelem') { // download link megszerzése
-        link += tlink[lecke.innerHTML];
-    } else {
-        link += mlink[lecke.innerHTML];
-    }
+    let link = `/osszefoglalok/${oldal}/${lecke.innerHTML}.pdf`;
     if (mobil()) {
-    console.log(link);
-
-    async function PDFLetoltes() {
-        const valasz = await fetch(link);
-
-        if (!valasz.ok) {
-            console.log("Download failed:", valasz.status);
-            return;
-        }
-
-        const blob = await valasz.blob();
-        const blobURL = URL.createObjectURL(blob);
-
-        const a = document.createElement("a");
-        a.href = blobURL;
-        a.download = "A 19. század második felének magyar irodalma.pdf";
-
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-
-        URL.revokeObjectURL(blobURL);
-    }
-
-    PDFLetoltes();
+        PDFLetoltes(link, lecke.innerHTML);
     }
     else {
         if (bezarva) {
@@ -80,9 +36,8 @@ function createFileExplorer(lecke){
             html_code += 'height="100%" width="100%" frameBorder="0" scrolling="auto"></iframe> <h5>Előnézet</h5> </div> <div class="column text-center">';
             html_code += '<h4>Összefoglaló címe:<br>' + lecke.innerHTML + "</h4>";
             html_code += '<button class="btn btn-primary" onclick="window.open(&#39;' + oldal + "/" + lecke.innerHTML + '.pdf&#39;)"><i class="fa-solid fa-file"></i> Megnyitás</button>';
-            html_code += '<button class="btn btn-success"><i class="fa-solid fa-arrow-down-to-line"></i><a href=';
-            html_code += link;
-            html_code += '> Letöltés</a></button>';
+            html_code += `<button class="btn btn-success" onclick="PDFLetoltes('${link}','${lecke.innerHTML}')"><i class="fa-solid fa-arrow-down-to-line"></i>`;
+            html_code += 'Letöltés</a></button>';
             html_code += '<p><span id="figyelem">FIGYELEM!</span> <br>A témazáró napjáig több, újabb verzió jelenhet meg, <br>így nem ajánlott semelyik fájl letöltése.</p>'
             html_code += '</div></div></div>'
             $("footer").before(html_code);
